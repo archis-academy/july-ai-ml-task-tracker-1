f0897f156757f2b942dfe4d66bdddd933da6a5cb
Ozcan Sert <ozcsert0@gmail.com>
2024-07-18 15:12:51 +0300

    JU-28 display_tasks & def added

--- task_manager.py
+++ task_manager.py
@@ -1,19 +1,22 @@
 class TaskManager:
 
-    def __init__(self):
-        self.tasks = {1: "taks1", 2: "task2"}  # {id,object}
-
-
+    def __init__(self): 
+        self.tasks = {
+            1: {"title": "Task 1", "description": "Description of task 1", "category": "Front-end"},
+            2: {"title": "Task 2", "description": "Description of task 2", "category": None}
+        } # test amacli dummy tasks 
+        
 
     def display_tasks(self):
-        for value in self.tasks.values():
-            print(value)
+        task_count = len(self.tasks)
+        print (f"Total Task Count:{task_count}")
 
-
-
+        for task_id, task_info in self.tasks.items():
+            print(f"Task ID: {task_id}")
+            for key, value in task_info.items():
+                    print(f"{key.capitalize()}: {value}")
 
 task_manager = TaskManager()
 
-
-
+# fonksiyonlari konsola bu sekilde cagirabilirsiniz.
 task_manager.display_tasks()
