2c936bda25f07169f54cfaf8045e22f2da5d1f6c
Ozcan Sert <ozcsert0@gmail.com>
2024-07-18 14:20:40 +0300

    JU-28 display tasks def eklendi

--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,19 @@
 class TaskManager:
 
     def __init__(self):
-        self.tasks = {}  # {id,object}
+        self.tasks = {1: "taks1", 2: "task2"}  # {id,object}
+
+
+
+    def display_tasks(self):
+        for value in self.tasks.values():
+            print(value)
+
+
+
+
+task_manager = TaskManager()
+
+
+
+task_manager.display_tasks()
