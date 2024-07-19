8dc881ce56e75343b86a2402923e87c33b1f8bcd
Ozcan Sert <ozcsert0@gmail.com>
2024-07-19 15:26:51 +0300

    removed test objects

--- task_manager.py
+++ task_manager.py
@@ -1,14 +1,10 @@
 class TaskManager:
 
     def __init__(self): 
-        self.tasks = {
-            1: {"title": "Task 1", "description": "Description of task 1", "category": "Front-end", "completed": True},
-            2: {"title": "Task 2", "description": "Description of task 2", "category": None, "completed": False},
-            3: {"title": "Task 3", "description": "Description of task 3", "category": "Backend" , "completed": True}
-        } # test amacli dummy tasks 
+        self.tasks = {} # {id,object}
         
 
-    def display_tasks(self):
+    def display_task_counts(self):
         task_count = len(self.tasks)
         print (f"Total Task Count:{task_count}")
 
@@ -16,13 +12,9 @@ class TaskManager:
         print(f"Completed Count: {completed_count}")
         print() #for clarity
 
-        for task_id, task_info in self.tasks.items():
-            print(f"Task ID: {task_id}")
-            for key, value in task_info.items():
+    def display_task_details(self):
+            for task_id, task_info in self.tasks.items():
+                print(f"Task ID: {task_id}")
+                for key, value in task_info.items():
                     print(f"{key.capitalize()}: {value}")
             
-    
-task_manager = TaskManager()
-
-# fonksiyonlari konsola bu sekilde cagirabilirsiniz.
-task_manager.display_tasks()
