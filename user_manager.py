92704403cf7fddc32cd7e9a62273aacc0fb79671
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-07-28 17:34:25 +0300

    [JU-16]-IUL editing finish

--- user_manager.py
+++ user_manager.py
@@ -3,3 +3,10 @@ class UserManager:
     def __init__(self):
         self.users = {}  # key value {username,user_object}
 
+def authenticate_user(self,username,password,):
+    user = self.users.get(username)
+    if user and user.password == password:
+        print("User authenticated successfully")
+        return user
+    print("Please check your username password")
+    return None
